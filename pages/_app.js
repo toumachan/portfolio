12e4e4d408799a98771210be62d440ad8e491a3e
Toma <75118476+toumachan@users.noreply.github.com>
2023-05-20 21:19:03 +0900

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+import '@/styles/reset.css'
+import '@/styles/header.css'
+import '@/styles/footer.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
